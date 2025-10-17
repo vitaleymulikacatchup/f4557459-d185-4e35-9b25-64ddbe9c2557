03f88d21a51c4f0459400d2bbbce4b6e668c74f0
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-17 17:21:55 +0300

    Update project files

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './external-1.css';
+import './external-2.css';
+import './external-3.css';
+import './inline-styles.css';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-1': '#1e2532',
+        'custom-2': '#5398ff',
+        'custom-3': '#ffca48',
+        'custom-4': '#1a49b9',
+        'custom-5': '#8f81fc',
+        'custom-6': '#ea4b71',
+        'custom-7': '#101330',
+        'custom-8': '#f4f7ff',
+        'custom-9': '#f3f3f3',
+        'custom-10': '#4b4b4b',
+        'custom-11': '#6fda44',
+        'custom-12': '#f3f5fa',
+        'custom-13': '#ffffff',
+        'custom-14': '#17313b',
+        'custom-15': '#ef4335'
+      },
+      fontFamily: {
+        'custom-1': ['system-ui', 'sans-serif']
+      }
+    }
+  }
+};
